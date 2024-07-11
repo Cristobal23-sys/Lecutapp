34499ff2b5155ee4224763856c3fde6da6bc22cc
Cristobal23-sys <cristobal.luna@inacapmail.cl>
2024-07-11 16:56:31 -0400

    PATCH v1.0.4

--- views/view-verlista.php
+++ views/view-verlista.php
@@ -50,7 +50,6 @@ while ($row = mysqli_fetch_assoc($resultReceta)) {
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -156,7 +155,7 @@ while ($row = mysqli_fetch_assoc($resultReceta)) {
 
 
   <div class="container" style="background-color: rgb(255, 255, 255); margin-top: 25px;">
-    <div class="row">
+    <div class="row justify-content-center">
       <?php
       // Verificar si el usuario tiene listas de deseos
       $sql = "SELECT c.id, c.producto_name AS producto_name, c.producto_url, c.producto_image AS producto_image, c.producto_categoria AS producto_categoria, c.producto_price AS producto_price, c.producto_url AS producto_url
@@ -177,24 +176,24 @@ while ($row = mysqli_fetch_assoc($resultReceta)) {
             $brand = $row['producto_categoria'];
             $url = $row['producto_url'];
             ?>
-            <div class="card mb-3 col-12" style="background-color: rgb(255, 255, 255); margin-top: 10px;">
+            <div class="card mb-4 col-12 col-md-8" style="background-color: rgb(255, 255, 255); margin-top: 10px;">
               <div class="row g-0 align-items-start">
                 <div class="col-md-4 col-12">
                   <img src="<?php echo $urlImagen ?>" class="img-fluid rounded-start card-image" alt="...">
                 </div>
                 <div class="col-md-8">
-                  <div class="card-body">
+                  <div class="card-body d-flex flex-column">
                     <h5 class="card-title" style="color: black; font-size: 1.0 rem;"><?php echo $name; ?></h5>
                     <p class="card-text" style="color: black; font-size: 0.8rem;"><?php echo $brand; ?></p>
                     <p class="card-title" style="color: black; font-size: 1.1rem;"><?php echo $price; ?></p>
-                  </div>
-                  <div class="mt-auto">
-                    <div class="card-footer d-flex flex-column justify-content-end align-items-end"
-                      style="background-color: transparent; border: none;">
-                      <a href="../Class/eliminarprodlista.php?idP=<?php echo $id ?>&idL=<?php echo $idL ?>"
-                        class="btn btn-danger mb-2" style="width: 200px;">Eliminar</a>
-                      <a href="<?php echo $url ?>" target="_blank" class="btn btn-primary" style="width: 200px;">Ir a la
-                        tienda</a>
+                    <div class="mt-auto">
+                      <div class="card-footer d-flex flex-column justify-content-end align-items-end"
+                        style="background-color: transparent; border: none;">
+                        <a href="../Class/eliminarprodlista.php?idP=<?php echo $id ?>&idL=<?php echo $idL ?>"
+                          class="btn btn-danger mb-2" style="width: 30%;">Eliminar</a>
+                        <a href="<?php echo $url ?>" target="_blank" class="btn btn-primary" style="width: 30%;">Ir a la
+                          tienda</a>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -215,7 +214,8 @@ while ($row = mysqli_fetch_assoc($resultReceta)) {
         <a href="../views/index.php" class="btn btn-success">Añadir productos</a>
       </div>
     </div>
-  </div>
+</div>
+
   <?php if (isset($alert_message)): ?>
     <script>
       alert("<?php echo $alert_message; ?>");
--- views/viewProducto.php
+++ views/viewProducto.php
@@ -63,6 +63,7 @@ if ($resultCategorias) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playwrite+ES+Deco:wght@100..400&display=swap" rel="stylesheet">
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
